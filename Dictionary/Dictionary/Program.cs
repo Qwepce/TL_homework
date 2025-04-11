a8d3882905f16bc6e98ad34acd918a712878dc18
Kirill Zinin <zinin.kirill.evgenievich@mail.ru>
2025-04-11 15:40:34 +0300

    рефакторинг кода, добавил новый класс TranslatorManager

--- Dictionary/Dictionary/Program.cs
+++ Dictionary/Dictionary/Program.cs
@@ -1,5 +1,4 @@
-﻿using Dictionary.Enums;
-using Dictionary.Implementations;
+﻿using Dictionary.Implementations;
 using Dictionary.Interfaces;
 using Dictionary.Utils;
 
@@ -11,64 +10,13 @@ public class Program
 
     public static void Main()
     {
-        ITranslatorService translatorService = new TranslatorService( FilePath );
         ICustomValidator validator = new CustomValidator();
-
-        string word;
-        string newWord;
-        string newTranslation;
+        ITranslatorService translatorService = new TranslatorService( FilePath );
 
         Console.WriteLine( Messages.GreetingMessage );
         Console.WriteLine( Messages.MenuMessage );
 
-        bool isExitSelected = false;
-
-        while ( !isExitSelected )
-        {
-            Console.Write( Messages.AskUserChoiceMessage );
-            int userCommandChoice = validator.GetValidUserChoice();
-
-            switch ( ( UserCommand )userCommandChoice )
-            {
-                case UserCommand.RussianToEnglish:
-                    word = validator.GetValidUserInput( Messages.AskUserInputMessage );
-
-                    translatorService.GetTranslation( word, isEnglishToRussian: false );
-                    break;
-
-                case UserCommand.EnglishToRussian:
-                    word = validator.GetValidUserInput( Messages.AskUserInputMessage );
-
-                    translatorService.GetTranslation( word, isEnglishToRussian: true );
-                    break;
-
-                case UserCommand.AddNewRussianWord:
-                    newWord = validator.GetValidUserInput( Messages.AskUserNewRussianWord );
-                    newTranslation = validator.GetValidUserInput( Messages.AskUserNewTranslation );
-
-                    translatorService.AddNewTranslation( newWord, newTranslation );
-                    break;
-
-                case UserCommand.AddNewEnglishWord:
-                    newTranslation = validator.GetValidUserInput( Messages.AskUserNewEnglishWord );
-                    newWord = validator.GetValidUserInput( Messages.AskUserNewTranslation );
-
-                    translatorService.AddNewTranslation( newWord, newTranslation );
-                    break;
-
-                case UserCommand.GetAllTranslations:
-                    translatorService.GetAllTranslations();
-                    break;
-
-                case UserCommand.Exit:
-                    isExitSelected = true;
-                    break;
-
-                default:
-                    Console.WriteLine( Messages.UnknownSelectedCommand );
-                    break;
-            }
-        }
+        TranslatorManager.Run( translatorService, validator );
 
         Console.WriteLine( Messages.FarewellMessage );
     }
---
+++ Dictionary/Dictionary/TranslatorManager.cs
@@ -0,0 +1,77 @@
+﻿using Dictionary.Enums;
+using Dictionary.Interfaces;
+using Dictionary.Utils;
+
+namespace Dictionary;
+
+public class TranslatorManager
+{
+    public static void Run( ITranslatorService translatorService, ICustomValidator validator )
+    {
+        bool isExitCommandExecuted = false;
+
+        while ( !isExitCommandExecuted )
+        {
+            Console.Write( Messages.AskUserChoiceMessage );
+            int userCommandChoice = validator.GetValidUserChoice();
+
+            isExitCommandExecuted = ExecuteCommand(
+                ( UserCommand )userCommandChoice,
+                translatorService,
+                validator );
+        }
+    }
+
+    private static bool ExecuteCommand(
+        UserCommand command,
+        ITranslatorService translatorService,
+        ICustomValidator validator )
+    {
+        string word;
+        string newWord;
+        string newTranslation;
+
+        switch ( command )
+        {
+            case UserCommand.RussianToEnglish:
+                word = validator.GetValidUserInput( Messages.AskUserInputMessage );
+
+                translatorService.GetTranslation( word, isEnglishToRussian: false );
+                break;
+
+            case UserCommand.EnglishToRussian:
+                word = validator.GetValidUserInput( Messages.AskUserInputMessage );
+
+                translatorService.GetTranslation( word, isEnglishToRussian: true );
+                break;
+
+            case UserCommand.AddNewRussianWord:
+                newWord = validator.GetValidUserInput( Messages.AskUserNewRussianWord );
+                newTranslation = validator.GetValidUserInput( Messages.AskUserNewTranslation );
+
+                translatorService.AddNewTranslation( newWord, newTranslation );
+                break;
+
+            case UserCommand.AddNewEnglishWord:
+                newTranslation = validator.GetValidUserInput( Messages.AskUserNewEnglishWord );
+                newWord = validator.GetValidUserInput( Messages.AskUserNewTranslation );
+
+                translatorService.AddNewTranslation( newWord, newTranslation );
+                break;
+
+            case UserCommand.GetAllTranslations:
+                translatorService.GetAllTranslations();
+
+                break;
+
+            case UserCommand.Exit:
+                return true;
+
+            default:
+                Console.WriteLine( Messages.UnknownSelectedCommand );
+                break;
+        }
+
+        return false;
+    }
+}
