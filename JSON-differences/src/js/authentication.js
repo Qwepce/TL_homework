2cdc259dc8ccd27f57a4ea3ee8de63a2c0143359
Kirill Zinin <zinin.kirill.evgenievich@mail.ru>
2025-05-14 17:40:43 +0300

    complete json-differences

---
+++ JSON-differences/src/js/authentication.js
@@ -0,0 +1,19 @@
+const login = (username) => {
+  if (!username || username.trim() === ``) {
+    return false;
+  }
+
+  localStorage.setItem(`username`, username.trim());
+  return true;
+};
+
+const logout = () => {
+  localStorage.removeItem(`username`);
+};
+
+const getAuthState = () => {
+  const username = localStorage.getItem("username");
+  return { isAuthenticated: !!username, username };
+};
+
+export const Authentication = { login, logout, getAuthState };
