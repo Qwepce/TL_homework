8d689495cace80544e404680fb9d39a7d24793af
Kirill Zinin <zinin.kirill.evgenievich@mail.ru>
2025-05-14 17:40:43 +0300

    complete json-differences

---
+++ JSON-differences/src/js/dom-elements.js
@@ -0,0 +1,25 @@
+export const elements = {
+  headerLogo: document.querySelector(`.header__logo-link`),
+
+  mainForm: document.querySelector(`.main-form-section`),
+  textareaOld: document.getElementById(`oldJson`),
+  textareaNew: document.getElementById(`newJson`),
+  mainFormButton: document.querySelector(`.main-form button`),
+
+  loginForm: document.querySelector(`.login-form`),
+  loginSection: document.querySelector(`.login-section`),
+  usernameInput: document.getElementById(`login`),
+
+  loginButton: document.querySelector(`.header__login-button`),
+  logoutButton: document.querySelector(`.logout-button`),
+  headerLoggedInContainer: document.querySelector(`.header__logged-in`),
+  headerUserName: document.getElementById(`username`),
+
+  promoSection: document.querySelector(`.promo-section`),
+  promoSectionLink: document.querySelector(`.promo-link`),
+
+  resultBlock: document.querySelector(`.result`),
+
+  oldJsonError: document.getElementById(`oldJson`).nextElementSibling,
+  newJsonError: document.getElementById(`newJson`).nextElementSibling,
+};
---
+++ JSON-differences/src/js/json-diff-handler.js
@@ -0,0 +1,47 @@
+import { JsonDiff } from "./json-diff.js";
+import { UI } from "./update-ui.js";
+import { elements } from "./dom-elements.js";
+import { JsonValidator } from "./json-validator.js";
+
+export function configureDiffHandler() {
+  elements.mainForm.addEventListener(`submit`, async (event) => {
+    event.preventDefault();
+    UI.hideJsonErrors();
+
+    let invalidInputs = false;
+    const oldJsonInput = elements.textareaOld.value.trim();
+    const newJsonInput = elements.textareaNew.value.trim();
+
+    const inputs = [
+      { value: oldJsonInput, errorElement: elements.oldJsonError },
+      { value: newJsonInput, errorElement: elements.newJsonError },
+    ];
+
+    inputs.forEach((input) => {
+      if (input.value === ``) {
+        UI.showJsonError(input.errorElement, `Обязательное поле`);
+        invalidInputs = true;
+      } else if (!JsonValidator.tryParse(input.value)) {
+        UI.showJsonError(input.errorElement, `Некорректный JSON`);
+        invalidInputs = true;
+      }
+    });
+
+    if (invalidInputs) {
+      return;
+    }
+
+    const defaultButtonHtml = elements.mainFormButton.innerHTML;
+    elements.mainFormButton.innerHTML = `Loading...`;
+    elements.mainFormButton.disabled = true;
+
+    const oldValue = JSON.parse(elements.textareaOld.value);
+    const newValue = JSON.parse(elements.textareaNew.value);
+    const result = await JsonDiff.create(oldValue, newValue);
+
+    elements.mainFormButton.innerHTML = defaultButtonHtml;
+    elements.mainFormButton.disabled = false;
+
+    UI.showResult(result);
+  });
+}
